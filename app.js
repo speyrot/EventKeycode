6b1dcbc17ab63840ca58f5d66c3c3a5b4b65206a
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-07-13 12:34:51 -0400

    wrote js

---
+++ app.js
@@ -0,0 +1,25 @@
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML =
+    
+    `
+    
+    <div class="key">
+        
+        ${event.key === ' ' ? 'Space' : event.key}
+
+        <small>event.keyCode</small>
+
+    </div>
+
+    <div class="key">
+        
+        ${event.code}
+
+        <small>event.code</small>
+    
+    </div> 
+    
+    `
+})
